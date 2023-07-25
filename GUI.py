d7c3c32072bdd726f56f0dffd0f4b9805f286c0a
Davinciam1 <ruslananushko2113@gmail.com>
2023-07-25 10:26:49 +0300

    add google trends loader (not stable)

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+import time
 import tkinter as tk
 import os
 import calendar
@@ -9,8 +10,10 @@ from tkcalendar import DateEntry
 import parser_amazon
 import patser_wb
 import hel10_sales_download as h10
+from gl_trends_req import Common_trends
 
 from datetime import datetime
+
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -36,8 +39,8 @@ class App(tk.Frame):
         self.rb01.grid(row=1, column=1, sticky=tk.N)
         self.rb02.grid(row=1, column=0, sticky=tk.N)
 
-        self.date_label= tk.Label(self, text="Load up to:")
-        self.date_label.grid(row=2, column=0,columnspan=3, sticky=tk.N)
+        self.date_label = tk.Label(self, text="Load up to:")
+        self.date_label.grid(row=2, column=0, columnspan=3, sticky=tk.N)
 
         self.last_day = tk.Label(self, text="Day:")
         self.last_day.grid(row=4, column=0, sticky=tk.W)
@@ -77,13 +80,70 @@ class App(tk.Frame):
 
         self.load_search_button = tk.Button(self, text="Load searches...", command=self.load_searches)
         self.load_search_button.grid(row=7, column=3)
+        self.new_window_button = tk.Button(self, text="Google Trends", command=self.open_new_window)
+        self.new_window_button.grid(row=6, column=3)
+
+    def open_new_window(self):
+        """Secondary window for loading trends from google trends"""
+        new_window = tk.Toplevel(self.master)
+        new_window.title("Google trends")
+        new_window.geometry("350x150")
+
+        self.asin_label  = tk.Label(new_window, text="KEYS:")
+        self.asin_label .grid(row=0, column=0, sticky=tk.W)
+        self.asin_entry = tk.Entry(new_window)
+        self.asin_entry.grid(row=0, column=1)
+        self.asin_button = tk.Button(new_window, text="Browse...", command=self.browse_asin_file)
+        self.asin_button.grid(row=0, column=2)
+
+        self.region = tk.Label(new_window, text="Region:")
+        self.region.grid(row=1, column=0, sticky=tk.W)
+        self.region_entry = tk.Entry(new_window)
+        self.region_entry.grid(row=1, column=1)
+
+        self.get_region_var = tk.BooleanVar()
+        self.get_region_checkbox = tk.Checkbutton(new_window, text="Keys in region", variable=self.get_region_var)
+        self.get_region_checkbox.grid(row=2, column=0, sticky=tk.W)
+
+        self.get_global_var = tk.BooleanVar()
+        self.get_global_checkbox = tk.Checkbutton(new_window, text="Keys by country", variable=self.get_global_var)
+        self.get_global_checkbox.grid(row=2, column=1, sticky=tk.W)
+
+        self.get_by_country_var = tk.BooleanVar()
+        self.get_by_country_checkbox = tk.Checkbutton(new_window, text="Keys by subregion",
+                                                      variable=self.get_by_country_var)
+        self.get_by_country_checkbox.grid(row=2, column=2, sticky=tk.W)
+
+        self.related_topics_var = tk.BooleanVar()
+        self.related_topics_checkbox = tk.Checkbutton(new_window, text="Related topics",
+                                                      variable=self.related_topics_var)
+        self.related_topics_checkbox.grid(row=3, column=0, sticky=tk.W)
+
+        self.related_searches_var = tk.BooleanVar()
+        self.related_searches_checkbox = tk.Checkbutton(new_window, text="Related searches",
+                                                        variable=self.related_searches_var)
+        self.related_searches_checkbox.grid(row=3, column=1, sticky=tk.W)
+
+        self.suggested_topics_var = tk.BooleanVar()
+        self.suggested_topics_checkbox = tk.Checkbutton(new_window, text="Suggested topics",
+                                                        variable=self.suggested_topics_var)
+        self.suggested_topics_checkbox.grid(row=3, column=2, sticky=tk.W)
+
+        self.save_label = tk.Label(new_window, text="Save directory:")
+        self.save_label.grid(row=4, column=0, sticky=tk.W)
+        self.save_entry = tk.Entry(new_window)
+        self.save_entry.grid(row=4, column=1)
+        self.save_button = tk.Button(new_window, text="Browse...", command=self.browse_save_directory)
+        self.save_button.grid(row=4, column=2)
+
+        self.load_trends_button = tk.Button(new_window, text="Load SKU", command=self.load_trends)
+        self.load_trends_button.grid(row=5, column=1, sticky=tk.S)
 
     def update_human_button_state(self):
         if h10.human_inter:  # Условие для включения/выключения кнопки
             self.human_interraction.config(state=tk.NORMAL)  # Включить кнопку
         else:
             self.human_interraction.config(state=tk.DISABLED)  # Выключить кнопку
-
 
     def load_sales_thread(self):
         threading.Thread(target=self.load_sales).start()
@@ -106,17 +166,17 @@ class App(tk.Frame):
 
 
     def load_feedbacks(self):
-        asin_list=self.asin_entry.get()
-        day=self.last_day_entry.get()
+        asin_list = self.asin_entry.get()
+        day = self.last_day_entry.get()
         if day == '':
-            day='1'
-        month=self.last_mounth_entry.get()
+            day = '1'
+        month = self.last_mounth_entry.get()
         if month == '':
-            month='1'
+            month = '1'
         month = calendar.month_name[int(month)]
-        year=self.last_year_entry.get()
-        end_date=day + " " + month + " " + year
-        filename=self.save_entry.get()+'/'+self.cat_entry.get()
+        year = self.last_year_entry.get()
+        end_date = day + " " + month + " " + year
+        filename = self.save_entry.get() + '/' + self.cat_entry.get()
 
         if os.path.isfile(asin_list):
             if os.path.splitext(asin_list)[1] == '.csv':
@@ -125,12 +185,12 @@ class App(tk.Frame):
                 asin_frame = pd.read_excel(asin_list)
             asin_list = list(asin_frame['Asin'])
         else:
-            asin_list=[asin_list]
+            asin_list = [asin_list]
 
         if self.az_wb_var.get() == 'az':
-            parser=parser_amazon
+            parser = parser_amazon
         elif self.az_wb_var.get() == 'wb':
-            parser=patser_wb
+            parser = patser_wb
         parser.scrap_feedbaks(sku_list=asin_list, end_date=end_date, filename=filename)
 
     def load_sales(self):
@@ -142,7 +202,7 @@ class App(tk.Frame):
                 asin_frame = pd.read_excel(asin_list)
             asin_list = list(asin_frame['Asin'])
         else:
-            asin_list=[asin_list]
+            asin_list = [asin_list]
         h10.load_sales(asin_list=asin_list)
 
     def load_searches(self):
@@ -156,8 +216,51 @@ class App(tk.Frame):
         else:
             key_list = [key_list]
         h10.load_searches(key_list=key_list, save_directory=self.save_entry.get())
+
+    def load_trends(self):
+        region = self.region_entry.get()
+        if region == '':
+            region = 'US'
+        by_region = self.get_region_var.get()
+        by_global = self.get_global_var.get()
+        by_country = self.get_by_country_var.get()
+        related_topics = self.related_topics_var.get()
+        related_searches = self.related_searches_var.get()
+        suggested_topics = self.suggested_topics_var.get()
+        savedir = self.save_entry.get()
+        key_list = self.asin_entry.get()
+        if os.path.isfile(key_list):
+            if os.path.splitext(key_list)[1] == '.csv':
+                key_frame = pd.read_csv(key_list, delimiter=';')
+            elif os.path.splitext(key_list)[1] == '.xlsx':
+                key_frame = pd.read_excel(key_list)
+            key_list = list(key_frame['Key'])
+        else:
+            key_list = [key_list]
+        ct = Common_trends(kw_list=key_list, region=region, savedir=savedir)
+        if by_region:
+            ct.get_region()
+            time.sleep(10)
+        if by_global:
+            ct = Common_trends(kw_list=key_list, region=region, savedir=savedir)
+            ct.get_global()
+            time.sleep(10)
+        if by_country:
+            ct.get_by_country()
+            time.sleep(10)
+        if related_topics:
+            ct.related_topics()
+            time.sleep(10)
+        if related_searches:
+            ct.related_searches()
+            time.sleep(10)
+        if suggested_topics:
+            ct.suggested_topics()
+            time.sleep(10)
+
     def human_ready(self):
-        h10.human_inter=False
+        h10.human_inter = False
+
 
 root = tk.Tk()
 root.title("Parser App")
---
+++ gl_trends_req.py
@@ -0,0 +1,167 @@
+import contextlib
+import time
+import requests
+import pandas as pd
+from pytrends.request import TrendReq
+from selenium import webdriver
+
+requests_args = {
+    'headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/108.0.0.0 Safari/537.36'
+    }
+}
+
+
+class _TrendReq(TrendReq):
+    pass
+    # ... добавьте вашу логику, если необходимо ...
+
+
+@contextlib.contextmanager
+def _requests_get_as_post():
+    requests.get, requests_get = requests.post, requests.get
+    try:
+        yield
+    finally:
+        requests.get = requests_get
+
+
+# Используйте _TrendReq вместо TrendReq
+with _requests_get_as_post():
+    pytrends = _TrendReq()
+
+
+# pt = TrendReq(hl="en-US", tz=360)
+# kw_list = ["патчи для глаз"]
+# pt.build_payload(kw_list, timeframe='today 5-y', geo='RU', gprop='')
+# massive = pt.interest_over_time()
+# print(massive.tail(20))
+
+
+class Common_trends:
+    def __init__(self, kw_list=[], timeframe='today 5-y', region='US', savedir=None):
+        self.pt = TrendReq(retries=5)
+
+        # self.pt = TrendReq(hl="en-US", tz=360,requests_args=requests_args)
+        self.kw_list = kw_list
+        self.timeframe = timeframe
+        self.region = region
+        self.savedir = savedir + '\\'
+
+    def get_region(self):
+        massive = pd.DataFrame()
+        if len(self.kw_list) > 5:
+            blocks = [self.kw_list[i:i + 5] for i in range(0, len(self.kw_list), 5)]
+            for i in range(len(blocks)):
+                self.pt.build_payload(blocks[i], timeframe=self.timeframe, geo=self.region, gprop='')
+                temp = self.pt.interest_over_time()
+                if massive.empty:
+                    massive = temp
+                else:
+                    massive = pd.concat([massive, temp], axis=1)
+                time.sleep(10)
+        else:
+            self.pt.build_payload(self.kw_list, timeframe=self.timeframe, geo=self.region, gprop='')
+            massive = self.pt.interest_over_time()
+        massive.to_excel(self.savedir + self.region+'_trends.xlsx')
+
+    def get_global(self):
+        by_country = pd.DataFrame()
+        if len(self.kw_list) > 5:
+            blocks = [self.kw_list[i:i + 5] for i in range(0, len(self.kw_list), 5)]
+            for i in range(len(blocks)):
+                self.pt.build_payload(blocks[i], timeframe=self.timeframe, gprop='')
+                temp = self.pt.interest_by_region("COUNTRY", inc_low_vol=True, inc_geo_code=True)
+                # by_country = pd.merge(by_country, temp, left_index=True, right_index=True)
+                if by_country.empty:
+                    by_country = temp
+                else:
+                    by_country = pd.concat([by_country, temp], axis=1)
+                time.sleep(10)
+        else:
+            self.pt.build_payload(self.kw_list, timeframe=self.timeframe, gprop='')
+            by_country = self.pt.interest_by_region("COUNTRY", inc_low_vol=True, inc_geo_code=True)
+        by_country.to_excel(self.savedir + 'intr_by_country.xlsx')
+
+    def get_by_country(self):
+        by_city = pd.DataFrame()
+        by_region = pd.DataFrame()
+        by_dma = pd.DataFrame()
+        if len(self.kw_list) > 5:
+            blocks = [self.kw_list[i:i + 5] for i in range(0, len(self.kw_list), 5)]
+            for i in range(len(blocks)):
+                self.pt.build_payload(blocks[i], timeframe=self.timeframe, geo=self.region, gprop='')
+                temp_city = self.pt.interest_by_region("CITY", inc_low_vol=True, inc_geo_code=True)
+                temp_region = self.pt.interest_by_region("REGION", inc_low_vol=True, inc_geo_code=True)
+                temp_dma = self.pt.interest_by_region("DMA", inc_low_vol=True, inc_geo_code=True)
+                if by_city.empty:
+                    by_city = temp_city
+                    by_region = temp_region
+                    by_dma = temp_dma
+                else:
+                    by_city = pd.merge(by_city, temp_city, left_index=True, right_index=True)
+                    by_region = pd.merge(by_region, temp_region, left_index=True, right_index=True)
+                    by_dma = pd.merge(by_dma, temp_dma, left_index=True, right_index=True)
+        else:
+            self.pt.build_payload(self.kw_list, timeframe=self.timeframe, geo=self.region, gprop='')
+            by_city = self.pt.interest_by_region("CITY", inc_low_vol=True, inc_geo_code=True)
+            by_region = self.pt.interest_by_region("REGION", inc_low_vol=True, inc_geo_code=True)
+            by_dma = self.pt.interest_by_region("DMA", inc_low_vol=True, inc_geo_code=True)
+        with pd.ExcelWriter(self.savedir + 'trends_by_county.xlsx', engine='xlsxwriter') as writer:
+            by_region.to_excel(writer, sheet_name='Region', index=True)
+            by_city.to_excel(writer, sheet_name='City', index=True)
+            by_dma.to_excel(writer, sheet_name='DMA', index=True)
+
+    def related_topics(self, ):
+        rising = pd.DataFrame()
+        top = pd.DataFrame()
+        for kw in self.kw_list:
+            kw_list = []
+            kw_list.append(kw)
+            self.pt.build_payload(kw_list, timeframe=self.timeframe, geo=self.region, gprop='')
+            rt = self.pt.related_topics()
+            rt[kw]['rising']['key'] = kw
+            rising = pd.concat([rising, rt[kw]['rising']], ignore_index=True)
+            rt[kw]['top']['key'] = kw
+            top = pd.concat([rising, rt[kw]['top']], ignore_index=True)
+        with pd.ExcelWriter(self.savedir + 'Related topics.xlsx', engine='xlsxwriter') as writer:
+            rising.to_excel(writer, sheet_name='rising', index=True)
+            top.to_excel(writer, sheet_name='top', index=True)
+
+        # self.pt.build_payload(self.kw_list, timeframe=self.timeframe, geo=self.region, gprop='')
+
+    def related_searches(self):
+        rising = pd.DataFrame()
+        top = pd.DataFrame()
+        for kw in self.kw_list:
+            kw_list = []
+            kw_list.append(kw)
+
+            self.pt.build_payload(kw_list, timeframe=self.timeframe, geo=self.region, gprop='')
+            rt = self.pt.related_queries()
+            if rt[kw]['rising'] is not None:
+                rt[kw]['rising']['key'] = kw
+                rising = pd.concat([rising, rt[kw]['rising']], ignore_index=True)
+            if rt[kw]['top']is not None:
+                rt[kw]['top']['key'] = kw
+                top = pd.concat([rising, rt[kw]['top']], ignore_index=True)
+        with pd.ExcelWriter(self.savedir + 'Related searches '+'.xlsx', engine='xlsxwriter') as writer:
+            rising.to_excel(writer, sheet_name='rising', index=True)
+            top.to_excel(writer, sheet_name='top', index=True)
+
+        # self.pt.build_payload(self.kw_list, timeframe=self.timeframe, geo=self.region, gprop='')
+
+    def suggested_topics(self):
+        sugg = pd.DataFrame()
+        for kw in self.kw_list:
+            topics = self.pt.suggestions(kw)
+            temp = pd.DataFrame(topics)
+            temp['key'] = kw
+            sugg = pd.concat([sugg, temp], ignore_index=True)
+        sugg.to_excel(self.savedir + 'Suggested topics.xlsx')
+
+# test = Common_trends(kw_list=['iphone', 'ios'],savedir='C:\\Users\\aos.user5\\Desktop')
+# test.get_region()
+# test.related_searches()
+# test.suggested_topics()
